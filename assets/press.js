e89b30a7675ebdf5fcbfec50940d9a77f153b5f0
Shoaib Tayyab <dezainer2002@gmail.com>
2021-03-10 16:02:30 +0500

    save changes

--- assets/press.js
+++ assets/press.js
@@ -12,4 +12,23 @@ window.addEventListener('load', function() {
       // $( '.pressSlider' ).show();
     });
   });
+
+  $(document)
+    .on('click', '.showMoreOrLess .showMoreOrLess_link', function ( e ) {
+      e.stopImmediatePropagation();
+
+      const wrapperSelector   =   $( '.pressAboutRotimatic_wrapper' ).attr( 'state' );
+      const selector          =   $( '.press_aboutRotimatic .pressAboutRotimatic_wrapper .pressAboutRotimatic_Item[state]' );
+
+      if ( wrapperSelector == 'hide' ) {
+        selector.fadeIn( '500' );
+        $( '.pressAboutRotimatic_wrapper' ).attr( 'state', 'show' );
+        $( this ).text( 'Show Less' );
+      } else if ( wrapperSelector == 'show' ) {
+        selector.fadeOut( '500' );
+        $( '.pressAboutRotimatic_wrapper' ).attr( 'state', 'hide' );
+        $( this ).text( 'Show More' );
+      }
+
+    });
 });
